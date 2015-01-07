8b15740f886499eecefb6bbcfcd041c578a48c39
Leo Bauza <leob@njimedia.com>
2015-01-06 22:54:22 -0500

    [chore] Refactor rows includes

--- sites/all/themes/jf/templates/node/node--project.tpl.php
+++ sites/all/themes/jf/templates/node/node--project.tpl.php
@@ -20,27 +20,6 @@ $content_rows = $jf->loadCollectionItems($fields['field_content_row'], array(
   'field_image_caption',
 ));
 
-// field collection -> easy variables
-// probably not needed? Refactor rows.tpl.inc and rows--basic.tpl.inc
-$project_rows = array();
-foreach ($content_rows as $key => $item) {
-
-  // $val = $item['value'];
-  // $row = $content['field_content_row'][$key]['entity']['field_collection_item'][$val];
-  $row = $item;
-  $project_rows[$key] = array(
-    // 'img' => $jf->projectFieldValue($row, 'field_image_one'),
-    // 'img_two' => $jf->projectFieldValue($row, 'field_image_two'),
-    // 'text' => $jf->projectFieldValue($row, 'field_row_text'),
-    // 'caption' => $jf->projectFieldValue($row, 'field_image_caption'),
-    'img' => $row['field_image_one'],
-    'img_two' => $row['field_image_two'],
-    'text' => $row['field_row_text'],
-    'caption' => $row['field_image_caption'],
-  );
-
-}
-
 // images
 $intro_img = file_create_url($fields['field_intro_image']['uri']);
 
