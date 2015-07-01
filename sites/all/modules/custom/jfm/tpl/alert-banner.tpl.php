33a44d530e26e2de8e99028f505f7ccd4fc2a7d4
Leo Bauza <leob@njimedia.com>
2015-07-01 19:33:22 -0400

    task(banner) Better spacing on mobile

--- sites/all/modules/custom/jfm/tpl/alert-banner.tpl.php
+++ sites/all/modules/custom/jfm/tpl/alert-banner.tpl.php
@@ -7,6 +7,6 @@
 <?php if ($show): ?>
   <div style="background-color: <?php print $background; ?>" class="banner__inner">
     <a style="color: <?php print $colour; ?>" class="link -ignored" target="_blank" href="<?php print $url; ?>"><?php print $text; ?></a>
-    <a href="#" class="dismiss">&times;</a>
+    <a href="#" class="dismiss -ignored">&times;</a>
   </div>
 <?php endif; ?>
